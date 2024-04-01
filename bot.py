697a8116e3a86e5f5ac1635763ae3a302b1dd0e1
LittleTatsumi <eric_chesne@hotmail.fr>
2024-04-01 13:04:22 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import time
+import pyautogui as gui
+
+#récupération des coordonées des points
+
+"""
+k=0
+while k != 1:
+    time.sleep(1)
+    x, y = gui.position()
+    print(x,",",y)
+
+"""
+time.sleep(3)
+gui.keyDown('shift')
+gui.click(1782,679)
+gui.click(1706,665)
+gui.click(1619,575)
+gui.click(1727,554)
+gui.click(1692,517)
+gui.click(1657,433)
+gui.click(1667,378)
+gui.click(1262,322)
+gui.click(1186,323)
+gui.click(1148,316)
+gui.click(1193,371)
+#gui.click()
